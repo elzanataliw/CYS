306c5713b135e3d4647532ee0960c152aba6af21
elzanataliw <elza.natali.w@mail.ugm.ac.id>
2021-05-09 22:45:27 +0700

    added register

--- CYS/CYS/Register.Designer.cs
+++ CYS/CYS/Register.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CYS
 {
-    partial class Login
+    partial class Register
     {
         /// <summary>
         /// Required designer variable.
@@ -29,78 +29,15 @@ namespace CYS
         /// </summary>
         private void InitializeComponent()
         {
+            this.btHome = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
             this.tbUsername = new System.Windows.Forms.TextBox();
             this.tbPassword = new System.Windows.Forms.TextBox();
-            this.btSignin = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btSignup = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.btHome = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F);
-            this.label1.Location = new System.Drawing.Point(116, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(354, 57);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "hi, welcome back!";
-            // 
-            // tbUsername
-            // 
-            this.tbUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbUsername.Location = new System.Drawing.Point(180, 155);
-            this.tbUsername.Name = "tbUsername";
-            this.tbUsername.Size = new System.Drawing.Size(212, 34);
-            this.tbUsername.TabIndex = 1;
-            this.tbUsername.Text = "username";
-            this.tbUsername.TextChanged += new System.EventHandler(this.tbUsername_TextChanged);
-            // 
-            // tbPassword
-            // 
-            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPassword.Location = new System.Drawing.Point(180, 207);
-            this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(212, 34);
-            this.tbPassword.TabIndex = 2;
-            this.tbPassword.Text = "username";
-            this.tbPassword.UseSystemPasswordChar = true;
-            this.tbPassword.TextChanged += new System.EventHandler(this.tbPassword_TextChanged);
-            // 
-            // btSignin
-            // 
-            this.btSignin.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btSignin.Location = new System.Drawing.Point(239, 257);
-            this.btSignin.Name = "btSignin";
-            this.btSignin.Size = new System.Drawing.Size(98, 32);
-            this.btSignin.TabIndex = 3;
-            this.btSignin.Text = "sign in";
-            this.btSignin.UseVisualStyleBackColor = true;
-            this.btSignin.Click += new System.EventHandler(this.btSignin_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label2.Location = new System.Drawing.Point(137, 325);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(193, 23);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "dont have any account?";
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.linkLabel1.Location = new System.Drawing.Point(336, 325);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(105, 23);
-            this.linkLabel1.TabIndex = 5;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "sign up here";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // btHome
             // 
@@ -108,38 +45,114 @@ namespace CYS
             this.btHome.Location = new System.Drawing.Point(487, 347);
             this.btHome.Name = "btHome";
             this.btHome.Size = new System.Drawing.Size(69, 30);
-            this.btHome.TabIndex = 6;
+            this.btHome.TabIndex = 8;
             this.btHome.Text = "home";
             this.btHome.UseVisualStyleBackColor = true;
             this.btHome.Click += new System.EventHandler(this.btHome_Click);
             // 
-            // Login
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.label1.Location = new System.Drawing.Point(107, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 57);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "create your account";
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(183, 128);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(212, 34);
+            this.tbName.TabIndex = 9;
+            this.tbName.Text = "name";
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(183, 177);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(212, 34);
+            this.tbUsername.TabIndex = 10;
+            this.tbUsername.Text = "username";
+            this.tbUsername.TextChanged += new System.EventHandler(this.tbUsername_TextChanged);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(183, 228);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(212, 34);
+            this.tbPassword.TabIndex = 11;
+            this.tbPassword.Text = "password";
+            this.tbPassword.UseSystemPasswordChar = true;
+            this.tbPassword.UseWaitCursor = true;
+            this.tbPassword.TextChanged += new System.EventHandler(this.tbPassword_TextChanged);
+            // 
+            // btSignup
+            // 
+            this.btSignup.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btSignup.Location = new System.Drawing.Point(240, 274);
+            this.btSignup.Name = "btSignup";
+            this.btSignup.Size = new System.Drawing.Size(98, 32);
+            this.btSignup.TabIndex = 12;
+            this.btSignup.Text = "sign up";
+            this.btSignup.UseVisualStyleBackColor = true;
+            this.btSignup.Click += new System.EventHandler(this.btSignup_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.linkLabel1.Location = new System.Drawing.Point(304, 336);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(99, 23);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "sign in here";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.Location = new System.Drawing.Point(174, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "already sign up?";
+            // 
+            // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 403);
-            this.Controls.Add(this.btHome);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btSignin);
+            this.Controls.Add(this.btSignup);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btHome);
             this.Controls.Add(this.label1);
-            this.Name = "Login";
-            this.Text = "Login";
+            this.Name = "Register";
+            this.Text = "Register";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
         private System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.Button btHome;
+        private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.TextBox tbUsername;
         private System.Windows.Forms.TextBox tbPassword;
-        private System.Windows.Forms.Button btSignin;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btSignup;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button btHome;
+        private System.Windows.Forms.Label label2;
     }
 }
