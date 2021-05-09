e27e12cf37dcff5bc51ff50e75c538b9443f9f4a
yzdnaufan <ahyaznau@gmail.com>
2021-05-09 22:32:51 +0700

    init

---
+++ CYS/CYS/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYS.Models
+{
+    class User
+    {
+        
+        protected string _username;
+        protected string _password;
+    }
+}
