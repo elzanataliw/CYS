c2caeb49b0691265d18436b6e5ba01b38ca9f0eb
yzdnaufan <ahyaznau@gmail.com>
2021-06-12 20:36:18 +0700

    up form frame result

---
+++ CYS/CYS/Result.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CYS
+{
+    partial class Result
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelanjutnya = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSebelumnya = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelanjutnya
+            // 
+            this.btnSelanjutnya.Location = new System.Drawing.Point(549, 380);
+            this.btnSelanjutnya.Name = "btnSelanjutnya";
+            this.btnSelanjutnya.Size = new System.Drawing.Size(243, 104);
+            this.btnSelanjutnya.TabIndex = 14;
+            this.btnSelanjutnya.Text = "Selanjutnya";
+            this.btnSelanjutnya.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(364, 81);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(99, 46);
+            this.lblQuestion.TabIndex = 11;
+            this.lblQuestion.Text = "Hasil";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 24.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(288, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 57);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "SKIN QUIZ";
+            // 
+            // btnSebelumnya
+            // 
+            this.btnSebelumnya.Location = new System.Drawing.Point(79, 390);
+            this.btnSebelumnya.Name = "btnSebelumnya";
+            this.btnSebelumnya.Size = new System.Drawing.Size(243, 104);
+            this.btnSebelumnya.TabIndex = 13;
+            this.btnSebelumnya.Text = "Sebelumnya";
+            this.btnSebelumnya.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(290, 171);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(263, 46);
+            this.lblName.TabIndex = 11;
+            this.lblName.Text = "lblNama Bahan";
+            this.lblName.Visible = false;
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc.Location = new System.Drawing.Point(349, 217);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(135, 46);
+            this.lblDesc.TabIndex = 11;
+            this.lblDesc.Text = "lblDesc";
+            this.lblDesc.Visible = false;
+            // 
+            // Result
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 506);
+            this.Controls.Add(this.btnSebelumnya);
+            this.Controls.Add(this.btnSelanjutnya);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.label1);
+            this.Name = "Result";
+            this.Text = "Result";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelanjutnya;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSebelumnya;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
